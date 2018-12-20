d7cb5e3f0fa2e702dc8820a5b4356f4be0d6a830
spcorum <spcorum@gmail.com>
2018-12-19 19:04:51 -0600

    Added unit tests for covar_fgn

--- R/covar_fgn.R
+++ R/covar_fgn.R
@@ -10,9 +10,10 @@
 #' \code{h}.
 #' @export
 covar_fgn <- function(q, h = 0.9) {
-  stopifnot(q %% 1 == 0, q > 0, h > 0, h < 1)
+  stopifnot(q %% 1 == 0, q > 0, h >= 0, h < 1)
   Sigma <- function(i, j, h) 0.5 * ((abs(i - j) + 1)^(2 * h) - 2 * (abs(i - j))^(2 * h) +
-      (abs(i - j) - 1)^(2 * h))
+    # (abs(i - j) - 1)^(2 * h))
+    ((abs(i - j) - 1)^2)^h)
   X <- diag(q)
   for (i in 1:q - 1) {
     for (j in ifelse(i < q, i + 1, i):q) {
